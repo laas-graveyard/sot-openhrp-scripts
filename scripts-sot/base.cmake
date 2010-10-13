d2d7017e634a3d34e8837cf9dd8918bcacf78428
Thomas Moulard <thomas.moulard@gmail.com>
2010-10-13 18:28:17 +0200

    Fix loadPlugins call in base.cmake.

--- scripts-sot/base.cmake
+++ scripts-sot/base.cmake
@@ -1,2 +1,2 @@
 loadPlugin shell-functions${DYN_LIB_EXT} ${CMAKE_INSTALL_PREFIX}/lib/plugin
-loadPlugins ${CMAKE_INSTALL_PREFIX}/script/${PLUGIN_LIST_FILENAME}
+loadPlugins ${DG_SCRIPT_DIR}/${PLUGIN_LIST_FILENAME}
