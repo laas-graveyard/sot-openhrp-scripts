00d6c796874d4aa86710a33e75b1bd12b87ade41
Thomas Moulard <thomas.moulard@gmail.com>
2010-10-14 19:00:25 +0200

    Add desktop shorcuts.

---
+++ share/applications/sot-openhrp.desktop.cmake
@@ -0,0 +1,8 @@
+[Desktop Entry]
+Type=Application
+Name=SoT OpenHRP
+GenericName=SoT OpenHRP
+Exec=@OPENHRP_HOME@/bin/unix/GrxUI.sh
+Path=@OPENHRP_HOME@/bin/unix
+Icon=openhrp
+Categories=Robotics;Science;Education;
