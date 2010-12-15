0a1fd5172e2ee0c00198e10ce97345d0c2bdbe0c
olivier-stasse <olivier.stasse@aist.go.jp>
2010-12-15 19:23:55 +0900

    Fix variables problem for some sot-scripts.

--- scripts-sot/dynfilessmallhrp2_10.cmake
+++ scripts-sot/dynfilessmallhrp2_10.cmake
@@ -1,3 +1,3 @@
-dyn2.setFiles ${HRP2_10-SMALL_DIRECTORY}/ HRP2JRLmainSmall.wrl ${HRP2_10-SMALL_DIRECTORY}/HRP2SpecificitiesSmall.xml ${HRP2_10-SMALL_DIRECTORY}/HRP2LinkJointRankSmall.xml
-dyn.setFiles ${HRP2_10-SMALL_DIRECTORY}/ HRP2JRLmainSmall.wrl ${HRP2_10-SMALL_DIRECTORY}/HRP2SpecificitiesSmall.xml ${HRP2_10-SMALL_DIRECTORY}/HRP2LinkJointRankSmall.xml
+dyn2.setFiles ${HRP2_10_DATAROOTDIR}/hrp2_10-small/ HRP2JRLmainSmall.wrl ${HRP2_10_DATAROOTDIR}/hrp2_10-small/HRP2SpecificitiesSmall.xml ${HRP2_10_DATAROOTDIR}/hrp2_10-small/HRP2LinkJointRankSmall.xml
+dyn.setFiles ${HRP2_10_DATAROOTDIR}/hrp2_10-small/ HRP2JRLmainSmall.wrl ${HRP2_10_DATAROOTDIR}/hrp2_10-small/HRP2SpecificitiesSmall.xml ${HRP2_10_DATAROOTDIR}/hrp2_10-small/HRP2LinkJointRankSmall.xml
 
