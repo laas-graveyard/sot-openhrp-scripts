676182b2eda10ebc663c1b05873d03f366e6a4f5
olivier-stasse <olivier.stasse@aist.go.jp>
2011-07-12 17:48:06 +0900

    Correct name problem with the small model.

--- scripts-sot/hwpgpginitpart1hrp2_10.cmake
+++ scripts-sot/hwpgpginitpart1hrp2_10.cmake
@@ -5,7 +5,7 @@ new PatternGenerator pg
 # Initialize files and information of the pattern generator.
 # pg.setParamPreview ${HRP2_10-SMALL_DIRECTORY}/PreviewControlParameters.ini
 pg.setVrmlDir ${HRP2_10_DATAROOTDIR}/hrp2-10-small/
-pg.setVrml HRP2JRLmainSmall.wrl
+pg.setVrml HRP2JRLmainsmall.wrl
 pg.setXmlSpec ${HRP2_10_DATAROOTDIR}/hrp2-10-small/HRP2SpecificitiesSmall.xml
 pg.setXmlRank ${HRP2_10_DATAROOTDIR}/hrp2-10-small/HRP2LinkJointRankSmall.xml
 
